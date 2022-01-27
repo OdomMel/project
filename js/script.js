cdc9d7a89e7b404c864f1b1c909152b63309c40b
ruslan <odommel21@gmail.com>
2022-01-27 14:39:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,109 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+// };
+
+// obj.a =10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     let result = 50;
+//     console.log(result);
+// }
+
+// let admin, name;
+
+// name = "John";
+
+// admin = name;
+
+// alert(admin);
+
+// let ourPlanetName = "Earth";
+
+// let currentUserName = "John";
+    
+// const BIRTHDAY = 21.08.1993;
+
+// const age = someCode();
+
+// let number, admin;
+
+// number = 99;
+
+// admin = number;
+
+// alert(admin);
+
+let age = prompt('How old are you?', 100);
+
+alert(`You ${age} year old!`);
+
+let Name = prompt('Who are you?', 'Your name here')
+
+alert(`Good day, ${Name}, have a nice day!`)
+
+
+// let a = +prompt('Первое число?', 1);
+// let b = +prompt('Второе число?', 2);
+
+// alert(a + b);
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('How old are you?',"too much");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('How old are you?', '');
+// answers[2] = prompt('Are you married?', '');
+
+// // document.write(answers);
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ruslan';
+
+// alert(`Hello, ${user}, how are you?`);
+
+// console.log('arr'+'- object');
+// console.log(4 + +'5');
+
+
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(--incr);
+// console.log(++decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+
+
+// const isChecked = true,
+//       isClosed = false;
+
+// console.log(isChecked || isClosed);
